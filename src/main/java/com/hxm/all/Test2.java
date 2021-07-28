3a058c3e48af1d012407d77b5f9005bb4f915563
linhao <haoxiangma@126.com>
2021-07-28 21:54:51 +0800

    version:3

---
+++ src/main/java/com/hxm/all/Test2.java
@@ -0,0 +1,12 @@
+package com.hxm.all;
+
+import java.sql.SQLOutput;
+
+/**
+ * @Author: linhao
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("version:3");
+    }
+}
